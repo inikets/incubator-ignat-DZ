320dbf07df93e3771bbee6bdaf6a87a7e0c2eaba
Nikita <inikets1@gmail.com>
2022-03-20 19:57:04 +0300

    update_7DZ

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HW7 from "../../h7/HW7";
+
+function Junior() {
+    return (
+        <div>
+            <HW7/>
+        </div>
+    )
+}
+
+export default Junior
